5c13eb985f8bbd4eec7fbd2cb227cafd682acdac
creativoma <ma.marianoalvarez@gmail.com>
2024-08-05 20:30:18 +0200

    chore: Remove unused code and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,93 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import { PDFViewer } from '@react-pdf/renderer';
-import MyDocument from './component';
+import React, { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+import MyDocument from "@/components/pdf/my-document";
+import { Input } from "@/components/ui/input";
+
+const PDFViewer = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
+  {
+    ssr: false,
+  },
+);
 
 const App = () => {
-  const [title, setTitle] = useState('Don Quijote de la Mancha');
-  const [author, setAuthor] = useState('Miguel de Cervantes');
-  const [content, setContent] = useState('En un lugar de la Mancha, de cuyo nombre no quiero acordarme...');
+  const [isClient, setIsClient] = useState(false);
+  const [args, setArgs] = useState({
+    title: "Don Quijote de la Mancha",
+    author: "Miguel de Cervantes",
+    content: "En un lugar de la Mancha, de cuyo nombre no quiero acordarme...",
+  });
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { id, value } = e.target;
+    setArgs((prevArgs) => ({
+      ...prevArgs,
+      [id]: value,
+    }));
+  };
 
   return (
     <div className="flex">
-      <form className='w-[calc(50vw)] h-screen p-4'>
+      <form className="w-[calc(50vw)] h-screen p-4">
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="title"
+          >
             Título
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="title"
             type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={args.title}
+            onChange={handleInputChange}
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="author">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="author"
+          >
             Autor
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="author"
             type="text"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            value={args.author}
+            onChange={handleInputChange}
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="content">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="content"
+          >
             Contenido
           </label>
           <textarea
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            value={args.content}
+            onChange={handleInputChange}
           />
         </div>
+        <Input type="email" placeholder="Email" />
       </form>
-      <PDFViewer className='w-[calc(50vw)] h-screen'>
-        <MyDocument title={title} author={author} content={content} />
-      </PDFViewer>
+      {isClient && (
+        <PDFViewer className="w-[calc(50vw)] h-screen">
+          <MyDocument args={args} />
+        </PDFViewer>
+      )}
     </div>
   );
 };
